45d74c994a416d3b0dbd24bfa7b8f70a3aba6d80
chanhliu <chanhliu1995@gmail.com>
2026-03-13 14:48:12 +0700

    start MCP Playwright before run healer

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
 
   // Retry on CI only
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 0 : 0,
 
   // Number of workers
   workers: process.env.CI ? 1 : undefined,
